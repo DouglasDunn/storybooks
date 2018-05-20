cfe996c16bf5d6920782e6289d8d808d5ceec072
Douglas Dunn <douglas.dunn@outlook.com>
2018-05-20 11:08:07 -0700

    configured google auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://dj:dj@ds129670.mlab.com:29670/storybooks-dev',
+  googleClientID:'404553449217-473hfggg4410cko08cjhh48k5vqe5u7r.apps.googleusercontent.com',
+  googleClientSecret:'y94E5fzq0M0GaAEMrdXWcn0h'
+}
